28a46a73297b9aba55c87b303d7dfc42a443b540
Benjamin Petit <bpetit@psykokwak.local>
2011-05-10 21:46:29 +0200

    Change from Cinder to Openframeworks

--- src/CameraPinhole.cpp
+++ src/CameraPinhole.cpp
@@ -1,42 +1,40 @@
 #include "CameraPinhole.h"
 
-OCVCamPinhole::OCVCamPinhole()
-{
-
-}
-
-OCVCamPinhole::OCVCamPinhole(const std::string matIntrinsicFile, const std::string matExtrinsicFile)
-{
-	mMatIntrinsic = (CvMat*) cvLoad(matIntrinsicFile.c_str());	
-	mMatIntrinsic = (CvMat*) cvLoad(matExtrinsicFile.c_str());	
-}
-
-namespace cinder{
+//OCVCamPinhole::OCVCamPinhole()
+//{
+//
+//}
+//
+//OCVCamPinhole::OCVCamPinhole(const std::string matIntrinsicFile, const std::string matExtrinsicFile)
+//{
+//	mMatIntrinsic = (CvMat*) cvLoad(matIntrinsicFile.c_str());	
+//	mMatIntrinsic = (CvMat*) cvLoad(matExtrinsicFile.c_str());	
+//}
 
 CameraPinhole::CameraPinhole() :
-	Camera()
+	ofCamera()
 {
 
 }
 
 CameraPinhole::CameraPinhole( int pixelWidth, int pixelHeight, float pixelFocal) :
-	Camera()
+	ofCamera()
 {
 	setPerspective( pixelWidth, pixelHeight, pixelFocal, pixelFocal, pixelWidth/2.0f, pixelHeight/2.0f, 0.0f);
 }
 
 CameraPinhole::CameraPinhole( int pixelWidth, int pixelHeight, float alphaU, float alphaV, float centerU, float centerV, float ditortionC) :
-	Camera()
+	ofCamera()
 {
 	setPerspective( pixelWidth, pixelHeight, alphaU, alphaV, centerU, centerV, ditortionC);
 }
 
-CameraPinhole::CameraPinhole( int pixelWidth, int pixelHeight, Matrix44f matIntrinsic, Matrix44f matExtrinsic) :
-	Camera()
-{
-	setPerspective( pixelWidth, pixelHeight, matIntrinsic[0,0], matIntrinsic[1,1], matIntrinsic[0,2], matIntrinsic[1,2], matIntrinsic[0,1]);
-
-}
+//CameraPinhole::CameraPinhole( int pixelWidth, int pixelHeight, Matrix44f matIntrinsic, Matrix44f matExtrinsic) :
+//	ofCamera()
+//{
+//	setPerspective( pixelWidth, pixelHeight, matIntrinsic[0,0], matIntrinsic[1,1], matIntrinsic[0,2], matIntrinsic[1,2], matIntrinsic[0,1]);
+//
+//}
 
 /*
 CameraPinhole::CameraPinhole( int pixelWidth, int pixelHeight, Matrix44f matProjection)
@@ -47,10 +45,10 @@ CameraPinhole::CameraPinhole( int pixelWidth, int pixelHeight, Matrix44f matProj
 
 void CameraPinhole::setPerspective( int pixelWidth, int pixelHeight, float alphaU, float alphaV, float centerU, float centerV, float ditortionC)
 {
-	mFov		= 2.0f * math<float>::tan( 2.0f * alphaU / (float)pixelWidth);
-	mAspectRatio 	= ( alphaV / alphaU ) * ( pixelWidth / (float)pixelHeight );
-	mNearClip 	= 0.1f;
-	mFarClip	= 10000.0f;
+	setFov(2.0f * tan( 2.0f * alphaU / (float)pixelWidth));
+	//aspectRatio 	= ( alphaV / alphaU ) * ( pixelWidth / (float)pixelHeight );
+	setNearClip(0.1f);
+	setFarClip(10000.0f);
 
 	mAlphaU 	= alphaU;
 	mAlphaV 	= alphaV;
@@ -64,48 +62,57 @@ void CameraPinhole::setPerspective( int pixelWidth, int pixelHeight, float alpha
 
 void CameraPinhole::calcProjection()
 {
-	float *m = mProjectionMatrix.m;
-	m[ 0] =  2.0f * mAlphaU / (float) mPixelW;
-	m[ 4] =  0.0f;
-	m[ 8] =  ( 2.0f * mCenterU / (float) mPixelW ) - 1.0f;
-	m[12] =  0.0f;
-
-	m[ 1] =  0.0f;
-	m[ 5] =  - 2.0f * mAlphaV / (float) mPixelH;
-	m[ 9] =  ( ( - 2.0f * mCenterV + 2.0f * mPixelH ) / mPixelH ) - 1.0f;
-	m[13] =  0.0f;
-
-	m[ 2] =  0.0f;
-	m[ 6] =  0.0f;
-	m[10] =  1.0f;
-	m[14] =  - mNearClip;
-
-	m[ 3] =  0.0f;
-	m[ 7] =  0.0f;
-	m[11] = 1.0f;
-	m[15] =  0.0f;
-/*
-	m = mInverseProjectionMatrix.m;
-	m[ 0] =  0.0f;
-	m[ 4] =  0.0f;
-	m[ 8] =  0.0f;
-	m[12] =  0.0f;
-
-	m[ 1] =  0.0f;
-	m[ 5] =  0.0f;
-	m[ 9] =  0.0f;
-	m[13] =  0.0f;
-
-	m[ 2] =  0.0f;
-	m[ 6] =  0.0f;
-	m[10] =  0.0f;
-	m[14] =  0.0f;
-
-	m[ 3] =  0.0f;
-	m[ 7] =  0.0f;
-	m[11] =  0.0f;
-	m[15] =  0.0f;
-*/
+	matProjection = ofMatrix4x4(
+2.0f * mAlphaU / (float) mPixelW,
+0.0f,
+0.0f,
+0.0f,
+	
+0.0f,
+- 2.0f * mAlphaV / (float) mPixelH,
+0.0f,	
+0.0f,
+	
+( 2.0f * mCenterU / (float) mPixelW ) - 1.0f,
+( ( - 2.0f * mCenterV + 2.0f * mPixelH ) / mPixelH ) - 1.0f,
+1.0f,	
+1.0f,
+	
+0.0f,
+0.0f,
+- nearClip,
+0.0f
+);
 }
 
-} // namespace cinder
+void CameraPinhole::begin(ofRectangle viewport)
+{
+	if(!isActive) ofPushView();
+	isActive = true;
+	
+	ofSetCoordHandedness(OF_RIGHT_HANDED);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixf(matProjection.getPtr());
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadMatrixf(ofMatrix4x4::getInverseOf(getGlobalTransformMatrix()).getPtr());
+
+	ofViewport(viewport);
+	
+	//store current matrices
+	if (cacheMatrices)
+	{
+		glGetFloatv(GL_PROJECTION_MATRIX, matProjection.getPtr());
+		glGetFloatv(GL_MODELVIEW_MATRIX, matModelView.getPtr());
+		hasStoredMatrices = true;
+	}
+}
+
+void CameraPinhole::end() {
+	if (isActive)
+	{
+		ofPopView();
+		isActive = false;
+	}
+}
--- src/CameraPinhole.h
+++ src/CameraPinhole.h
@@ -1,45 +1,47 @@
 #pragma once
 
-#include "cinder/Camera.h"
-#include "cv.h"
-#include "cxcore.h"
+#include "ofCamera.h"
 
-class OCVCamPinhole
-{
+//#include "cv.h"
+//#include "cxcore.h"
+//
+//class OCVCamPinhole
+//{
+//
+//public:
+//
+//	OCVCamPinhole();
+//	OCVCamPinhole(const std::string matIntrinsicFile, const std::string matExtrinsicFile);
+//	
+//protected:
+//
+//	CvMat* mMatIntrinsic;
+//	CvMat* mMatExtrinsic;	
+//	
+//
+//};
 
-public:
-
-	OCVCamPinhole();
-	OCVCamPinhole(const std::string matIntrinsicFile, const std::string matExtrinsicFile);
-	
-protected:
-
-	CvMat* mMatIntrinsic;
-	CvMat* mMatExtrinsic;	
-	
-
-};
-
-namespace cinder {
-
-class CameraPinhole : public Camera
+class CameraPinhole : public ofCamera
 {
 
 public:
 
 	CameraPinhole();
-        CameraPinhole( int pixelWidth, int pixelHeight, float alphaU, float alphaV, float centerU, float centerV, float ditortionC = 0);
+	CameraPinhole( int pixelWidth, int pixelHeight, float alphaU, float alphaV, float centerU, float centerV, float ditortionC = 0);
 	CameraPinhole( int pixelWidth, int pixelHeight, float pixelFocal);
-	CameraPinhole( int pixelWidth, int pixelHeight, Matrix44f matIntrinsic, Matrix44f matExtrinsic);
+	//CameraPinhole( int pixelWidth, int pixelHeight, Matrix44f matIntrinsic, Matrix44f matExtrinsic);
 	//CameraPinhole( int pixelWidth, int pixelHeight, Matrix44f matProjection);
 
-        void setPerspective(int pixelWidth, int pixelHeight, float alphaU, float alphaV, float centerU, float centerV, float ditortionC = 0);
+	void setPerspective(int pixelWidth, int pixelHeight, float alphaU, float alphaV, float centerU, float centerV, float ditortionC = 0);
 
-        virtual bool            isPersp() const { return true; }
+	//virtual bool            isPersp() const { return true; }
+	
+	virtual void begin(ofRectangle viewport = ofGetCurrentViewport());
+	virtual void end();
 	
 protected:
 	
-	virtual void            calcProjection();
+	virtual void calcProjection();
 	
 	float 	mAlphaU;
 	float 	mAlphaV;
@@ -48,5 +50,3 @@ protected:
 	int 	mPixelW;
 	int 	mPixelH;
 };
-
-} // namespace cinder
